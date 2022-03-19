ac3b8ade815df616062c1f80a87e61916989abc7
Patrick Huang <huangpatrick16777216@gmail.com>
2022-03-18 20:17:52 -0700

    Load kernels from paths.

--- src/driver/__main__.py
+++ src/driver/__main__.py
@@ -17,9 +17,26 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+import os
 import argparse
-from .kernel import Kernel
+from .kernel import Kernel, KernelException
 from . import logger
+
+
+def load_kernels(paths):
+    kernels = {}
+    for path in paths:
+        for dir in os.listdir(path):
+            p = os.path.join(path, dir)
+            if os.path.isdir(p):
+                try:
+                    k = Kernel(p)
+                    kernels[dir] = k
+                    logger.info(f"Loaded kernel: {dir}")
+                except (StopIteration, KernelException):
+                    logger.warn(f"Failed to load kernel: {dir}")
+
+    return kernels
 
 
 def main():
@@ -27,7 +44,8 @@ def main():
         description="Video rendering pipeline with piano visualization.\n"
                     "This program drives the rendering kernels.")
     parser.add_argument("-V", "--version", help="Print version.", action="store_true")
-    parser.add_argument("-p", "--paths", help="Path to kernel executables (sep=\":\").")
+    parser.add_argument("-p", "--paths", help="Path to kernel executables (sep=\":\").",
+        required=True)
     parser.add_argument("-v", "--verbose", help="Verbose output.", action="store_true")
     args = parser.parse_args()
 
@@ -36,15 +54,7 @@ def main():
         return
 
     logger.set_verbose(args.verbose)
-
-    from .utils import readall
-    k = Kernel(".")
-    print(k.dir_path, k.exe_path, k.lang)
-    proc = k.run()
-    proc.wait()
-    print(readall(proc.stdout))
-
-    #kernels = load_kernels(args.paths)
+    kernels = load_kernels(args.paths.split(os.path.pathsep))
 
 
 if __name__ == "__main__":
