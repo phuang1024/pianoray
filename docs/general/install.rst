8a1e4202b3f8cfdeffd640d03d468f772db62f0f
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2022-04-09 14:09:12 -0700

    Installation.

---
+++ docs/general/install.rst
@@ -0,0 +1,11 @@
+Installation
+============
+
+.. code-block:: sh
+
+   git clone https://github.com/phuang1024/pianoray
+   cd ./pianoray
+   make wheel
+   make install
+
+You can invoke with ``pianoray``.
--- src/main.py
+++ src/main.py
@@ -24,10 +24,14 @@ from .settings import Settings
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Piano performance visualizer.")
-    parser.add_argument("-s", "--settings", help="Path to settings JSON file.", required=True)
-    parser.add_argument("-o", "--output", help="Output video file.", required=True)
-    parser.add_argument("-c", "--cache", help="Cache path (default .prcache)", default=".prcache")
+    parser = argparse.ArgumentParser(
+        description="Piano performance visualizer.")
+    parser.add_argument("-s", "--settings",
+        help="Path to settings JSON file.", required=True)
+    parser.add_argument("-o", "--output",
+        help="Output video file.", required=True)
+    parser.add_argument("-c", "--cache",
+        help="Cache path (default .prcache)", default=".prcache")
     args = parser.parse_args()
 
     with open(args.settings, "r") as fp:
