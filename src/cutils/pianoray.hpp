dbaccbeb44354e92fd2ad1bf314a5ea3635d65e4
Patrick Huang <huangpatrick16777216@gmail.com>
2022-04-18 16:20:47 -0700

    Some piano utilities.

--- src/cutils/pianoray.hpp
+++ src/cutils/pianoray.hpp
@@ -19,6 +19,8 @@
 
 #pragma once
 
+#include <algorithm>
+
 
 namespace Pianoray {
 
@@ -73,6 +75,79 @@ public:
 double hypot(double x, double y) {
     return pow(x*x + y*y, 0.5);
 }
+
+int ibounds(int v, int min, int max) {
+    return std::min(std::max(v, min), max);
+}
+
+double dbounds(double v, double min, double max) {
+    return std::min(std::max(v, min), max);
+}
+
+
+/**
+ * If key is white.
+ */
+bool is_white_key(int key) {
+    int v = key % 12;
+    if (v == 1 || v == 4 || v == 6 || v == 9 || v == 11)
+        return false;
+    return true;
+}
+
+/**
+ * Position of the center of the key on the keyboard.
+ * @return  Factor from 0 to 1 (start of first key to end of last).
+ */
+double key_pos(int key) {
+    double white_width = 1 / 52;
+
+    bool last_white = false;  // Last key is white
+    double pos = 0;
+    for (int k = 0; k <= key; k++) {
+        bool white = is_white_key(k);
+        if (white && last_white)
+            pos += white_width;
+        else
+            pos += white_width / 2;
+        last_white = white;
+    }
+
+    return pos;
+}
+
+
+
+
+/*
+def key_coords(settings: Settings, key: int) -> Tuple[float, float]:
+    """
+    Horizontal (x) coordinates of key on the screen.
+    :param key: Key.
+    :return: ``(start_coord, end_coord)`` of key.
+    """
+    center = np.interp(key_pos(key), (0, 1), (0, settings.video.resolution[0]))
+    white_width = settings.video.resolution[0] / 52
+    black_width = white_width * settings.piano.black_width_fac
+    width = white_width if is_white_key(key) else black_width
+    half = width / 2
+    return (center-half, center+half)
+
+
+def note_coords(settings: Settings, event_frame: float,
+        frame: float) -> float:
+    """
+    Vertical (y) coordinates of an event as it is dropping from the top
+    to the keyboard.
+    :param event_frame: The time of the event.
+    :param frame: Current frame.
+    :return: Y pixel position.
+    """
+    height = settings.video.resolution[1] / 2
+    speed = (settings.blocks.speed * height / settings.video.fps)
+    delta = speed * (frame-event_frame)
+    return height + delta
+*/
 
 
 }  // namespace Pianoray
