2dc052378aa7b2522a71b1206c4f95a5534875a4
Patrick Huang <huangpatrick16777216@gmail.com>
2022-05-01 09:21:10 -0700

    Property base class methods.

--- pianoray/api/props.py
+++ pianoray/api/props.py
@@ -1,4 +1,30 @@
+from typing import Any, Sequence, Set
+
+
 class Property:
     """
     Property base class.
     """
+    name: str
+    description: str
+
+    animatable: bool
+    supported_interps: Sequence[str]
+
+    default: Any
+
+    _keyframes: Set[Keyframe]
+
+    def __init__(self, name: str, description: str, default: Any,
+            animatable: bool, supported_interps: Sequence[str]) -> None:
+        self.name = name
+        self.description = description
+        self.default = default
+        self.animatable = animatable
+        self.supported_interps = supported_interps
+
+        self._keyframes = set()
+
+    def animate(self, keyframe: Keyframe) -> None:
+        assert self.animatable
+        self._keyframes.add(keyframe)
