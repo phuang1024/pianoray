dd36df770bc78ce79faa7e2184b291af50246bb0
Patrick Huang <huangpatrick16777216@gmail.com>
2022-05-23 21:26:45 -0700

    Removed defaults.

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,17 @@
-<script>
-	export let name;
-</script>
-
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Text</h1>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        background-color: black;
+    }
 
 	h1 {
-		color: #ff3e00;
+        text-align: center;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		color: white;
 	}
 </style>
